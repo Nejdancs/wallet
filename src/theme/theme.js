7bb63840d9854691bad92ae86611a4e6561a2f82
Sergey Korobka <nejdancs@gmail.com>
2022-11-30 20:42:12 +0200

    add fonts and variables

---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  fontFamily: {
+    poppins: "'Poppins', sans-serif",
+    circe: "'Circe', sans-serif",
+  },
+  colors: {
+    accentPrimary: '#24CCA7',
+    accentSecondary: '#4A56E2',
+    primaryTextColor: '#000000',
+    secondaryTextColor: '#ffffff',
+    expenseColor: '#FF6596',
+    inActiveIconColor: '#6E78E8',
+    firstCircleColorBg: '#C5BAFF',
+    secondaryCircleColorBg: '#FFD8D0',
+  },
+};
+
+export default theme;
