58ff3984f93cd2c8c851676fd92cedf2276552a5
Sergey Korobka <nejdancs@gmail.com>
2022-12-01 23:09:42 +0200

    fix

--- src/assets/breakpoints/device.js
+++ src/assets/breakpoints/device.js
@@ -1,9 +1,9 @@
 import size from './size';
 
 const device = {
-  mobile: `(min-width: ${size.mobile})`,
-  tablet: `(min-width: ${size.tablet})`,
-  desktop: `(min-width: ${size.desktop})`,
+  mobile: `screen and (min-width: ${size.mobile})`,
+  tablet: `screen and (min-width: ${size.tablet})`,
+  desktop: `screen and (min-width: ${size.desktop})`,
 };
 
 export default device;
