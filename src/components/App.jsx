2b1c11ddb723c4387bd51245edc53905cd8e9547
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-12-01 15:41:30 +0200

    add loader, container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 // import { Navigate, Route, Routes } from 'react-router-dom';
 // import { lazy } from 'react';
+import Container from './Container/Container';
 import Balance from './Balance/Balance';
+import Loader from './Loader/Loader';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <Balance />
-          </>
-        }
-      />
+    <Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Balance />
+              <Loader />
+            </>
+          }
+        />
 
-      {/* <Route path="/" element={<DashboardPage />}>
+        {/* <Route path="/" element={<DashboardPage />}>
         <Route index element={<Navigate to="/home" />} />
         <Route path="/home" element={<HomeTab />} />
         <Route path="/diagram" element={<DiagramTab />} />
@@ -26,6 +30,7 @@ export const App = () => {
       <Route path="/signin" element={<SignIn />} />
 
       <Route path="*" element={<Navigate to="/" />} /> */}
-    </Routes>
+      </Routes>
+    </Container>
   );
 };
