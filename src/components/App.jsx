473b7f4b213023f15e480556611e9e28dc5b5785
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-12-01 12:49:12 +0200

    add balance component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
+import Balance from './Balance/Balance';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        fontWeight: 700,
-        color: '#010101',
-      }}
-    >
-      Wallet
-    </div>
+    <>
+      {' '}
+      {/* <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          fontWeight: 700,
+          color: '#010101',
+        }}
+      >
+        Wallet
+      </div> */}
+      <Balance />
+    </>
   );
 };
