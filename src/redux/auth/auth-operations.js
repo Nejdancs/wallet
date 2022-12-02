d57d892d1c63cd1eac1c7ffe4cf461ab95c592bb
nazar-yednak <93836409+nazar-yednak@users.noreply.github.com>
2022-12-02 22:40:26 +0200

    Додано код в redax та зроблено fix ModalLogout

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,13 +1,63 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const signUp = createAsyncThunk();
+axios.defaults.baseURL = '...';
 
-const logIn = createAsyncThunk();
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
 
-const logOut = createAsyncThunk();
+const signUp = createAsyncThunk('auth/register', async credentials => {
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-const fetchCurrentUser = createAsyncThunk();
+const logIn = createAsyncThunk('auth/login', async credentials => {
+  try {
+    const { data } = await axios.post('/users/login', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const logOut = createAsyncThunk('auth/logout', async () => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const fetchCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue();
+    }
+
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/users/current');
+      return data;
+    } catch (error) {}
+  }
+);
 
 const operations = {
   signUp,
