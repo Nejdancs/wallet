30d87c3cfcdb86cd967a6e159554774f30890839
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-12-06 14:20:16 +0200

    making fetch request through the backend
    let the comments and console logs remain until the final version . it's gonna be deleted then

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -7,7 +7,7 @@ import {
   CurrencyListHeader,
   HeaderText,
   CurrencyInfoText,
-} from './Currensy.styled';
+} from './Currency.styled';
 
 function Currency() {
   const [currency, setCurrency] = useState([]);
@@ -17,23 +17,26 @@ function Currency() {
   async function readFromLocalStorage() {
     const data = JSON.parse(localStorage.getItem('currencyData'));
     if (data && data.length === 3) {
-      setTimeout(() => {
-        setCurrency(data);
-      }, 1000);
+      // setTimeout(() => {
+      console.log('data is already in the Local Storage');
+      setCurrency(data);
+
+      // }, 1000);
     } else {
       const interval = setInterval(() => {
-        fetchCurrency();
         const data = JSON.parse(localStorage.getItem('currencyData'));
-        if (data) {
-          clearInterval(interval);
-
+        if (data && data.length > 0) {
           setCurrency(data);
-
+          console.log('this is console after data has been got');
+          clearInterval(interval);
           setTimeout(() => {
             localStorage.removeItem('currencyData');
           }, 600000);
+        } else {
+          console.log('data has not been found. calling fetch function');
+          fetchCurrency();
         }
-      }, 7000);
+      }, 3000);
     }
   }
 
@@ -41,9 +44,6 @@ function Currency() {
     readFromLocalStorage();
     const interval = setInterval(() => {
       readFromLocalStorage();
-      // fetchCurrency();
-      // const data = JSON.parse(localStorage.getItem('currencyData'));
-      // setCurrency(data);
     }, 1200000);
 
     return () => clearInterval(interval);
@@ -68,13 +68,13 @@ function Currency() {
                 </CurrencyInfoText>
                 <CurrencyInfoText>
                   {index === 0 || index === 1
-                    ? el.rateBuy.toFixed(2)
-                    : el.rateBuy}
+                    ? Number(el.rateBuy).toFixed(2)
+                    : Number(el.rateBuy).toFixed(3)}
                 </CurrencyInfoText>
                 <CurrencyInfoText>
                   {index === 0 || index === 1
-                    ? el.rateSell.toFixed(2)
-                    : el.rateSell}
+                    ? Number(el.rateSell).toFixed(2)
+                    : Number(el.rateSell).toFixed(3)}
                 </CurrencyInfoText>
               </CurrensyItems>
             ))}
@@ -84,3 +84,90 @@ function Currency() {
 }
 
 export default Currency;
+
+// import React, { useEffect, useState } from 'react';
+// import fetchCurrency from './CurrencyApi';
+// import {
+//   CurrensyItems,
+//   CurrencyList,
+//   CurrencyWrapper,
+//   CurrencyListHeader,
+//   HeaderText,
+//   CurrencyInfoText,
+// } from './Currency.styled';
+
+// function Currency() {
+//   const [currency, setCurrency] = useState([]);
+//   // const [loading, setLoading] = useState(false);
+//   // const [datasTime, setDatasTime] = useState('');
+
+//   async function readFromLocalStorage() {
+//     const data = JSON.parse(localStorage.getItem('currencyData'));
+//     if (data && data.length === 3) {
+//       setTimeout(() => {
+//         setCurrency(data);
+//       }, 1000);
+//     } else {
+//       const interval = setInterval(() => {
+//         fetchCurrency();
+//         const data = JSON.parse(localStorage.getItem('currencyData'));
+//         if (data) {
+//           clearInterval(interval);
+
+//           setCurrency(data);
+
+//           setTimeout(() => {
+//             localStorage.removeItem('currencyData');
+//           }, 120000);
+//         }
+//       }, 7000);
+//     }
+//   }
+
+//   useEffect(() => {
+//     readFromLocalStorage();
+//     const interval = setInterval(() => {
+//       readFromLocalStorage();
+//       // fetchCurrency();
+//       // const data = JSON.parse(localStorage.getItem('currencyData'));
+//       // setCurrency(data);
+//     }, 240000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <CurrencyWrapper>
+//       <CurrencyListHeader>
+//         <HeaderText>Currency</HeaderText>
+//         <HeaderText>Purchase</HeaderText>
+//         <HeaderText>Sale</HeaderText>
+//       </CurrencyListHeader>
+//       <CurrencyList>
+//         {currency.length === 0
+//           ? 'waiting for rates.......  some spinner'
+//           : currency?.map((el, index) => (
+//               <CurrensyItems key={`${index}${el.currencyCodeA}`}>
+//                 <CurrencyInfoText>
+//                   {index === 0 && 'USD'}
+//                   {index === 1 && 'EUR'}
+//                   {index === 2 && 'Є/$'}
+//                 </CurrencyInfoText>
+//                 <CurrencyInfoText>
+//                   {index === 0 || index === 1
+//                     ? el.rateBuy.toFixed(2)
+//                     : el.rateBuy}
+//                 </CurrencyInfoText>
+//                 <CurrencyInfoText>
+//                   {index === 0 || index === 1
+//                     ? el.rateSell.toFixed(2)
+//                     : el.rateSell}
+//                 </CurrencyInfoText>
+//               </CurrensyItems>
+//             ))}
+//       </CurrencyList>
+//     </CurrencyWrapper>
+//   );
+// }
+
+// export default Currency;
