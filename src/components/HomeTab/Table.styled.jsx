6848e3bead5e7e5a05e96d9c58e48802defb15fe
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-12-11 19:23:31 +0200

    fix pagination-button

--- src/components/HomeTab/Table.styled.jsx
+++ src/components/HomeTab/Table.styled.jsx
@@ -1,5 +1,5 @@
-import styled, { keyframes } from "styled-components";
-import device from "assets/breakpoints/device";
+import styled, { keyframes } from 'styled-components';
+import device from 'assets/breakpoints/device';
 
 export const PaginationContainer = styled.div`
   display: flex;
@@ -10,102 +10,101 @@ export const PaginationContainer = styled.div`
 `;
 
 export const PageContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const PaginationButton = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 150px;
-    height: 40px;
-    background-color: #fff;
-    border-radius: 6px;
-    border: none;
-    font-size: 14px;
-    font-weight: 500;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 40px;
 
-    &:hover {
-        background-color: #e0e0e0;
-    }
+  background-color: #fff;
+  border-radius: 30px;
+  border: 1px solid #a8a8ac;
+  box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
 
-    &:disabled {
-        background-color: #e0e0e0;
-        cursor: not-allowed;
-    }
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #4a56e2;
+    color: #ffffff;
+  }
+
+  &:disabled {
+    background-color: #e0e0e0;
+    cursor: not-allowed;
+  }
 `;
 
 export const PageCounterCont = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 16px;
-    font-weight: 500;
-    color: #757575;
-    margin-left: 20px;
-    margin-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 500;
+  color: #757575;
+  margin-left: 20px;
+  margin-right: 20px;
 `;
 
 export const PageCounter = styled.em`
-    font-size: 16px;
-    font-weight: 500;
-    color: #757575;
-    margin-left: 3px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #757575;
+  margin-left: 3px;
 `;
 
 export const SizeSelector = styled.select`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 130px;
-    height: 40px;
-    background-color: #fff;
-    border: none;
-    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
-    border-radius: 6px;
-    padding: 10px 20px;
-     outline: none;
-    font-size: 14px;
-    font-weight: 500;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    &:hover {
-        background-color: #e0e0e0;
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  width: 120px;
+  height: 40px;
+
+  border: 1px solid #a8a8ac;
+  border-radius: 30px;
+  box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
+
+  background-color: #fff;
+
+  outline: none;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+
+  transition: all 0.3s ease;
+
+  &:hover {
+    border: 1px solid #4a56e2;
+  }
 `;
 
 export const SelectOption = styled.option`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 130px;
-    height: 40px;
-    background-color: #fff;
-    border: 1px solid #e0e0e0;
-    border-radius: 4px;
-    padding: 10px 20px;
-    font-size: 14px;
-    font-weight: 500;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    &:hover {
-        background-color: #e0e0e0;
-    }
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  width: 130px;
+  height: 40px;
 
-export const ButtonImgLeft = styled.img`
-    width: 15px;
-    height: 15px;
-    margin-right: 10px;
-`;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
 
-export const ButtonImgRight = styled.img`
-    width: 15px;
-    height: 15px;
-    margin-left: 10px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
 `;
