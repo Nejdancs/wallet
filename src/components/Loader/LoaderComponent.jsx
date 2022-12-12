45447d83b13d2fca11d9a69541a04f5f0af51058
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-12-12 18:37:53 +0200

    fix loader

--- src/components/Loader/LoaderComponent.jsx
+++ src/components/Loader/LoaderComponent.jsx
@@ -1,20 +1,20 @@
-import { ThreeCircles } from 'react-loader-spinner';
+import { Oval } from 'react-loader-spinner';
 import { LoaderComponentWrapper } from './Loader.styled';
 
 const LoaderComponent = () => {
   return (
     <LoaderComponentWrapper>
-      <ThreeCircles
-        height="50"
-        width="50"
-        color="#FF6596"
+      <Oval
+        height={50}
+        width={50}
+        color="#4A56E2"
         wrapperStyle={{}}
         wrapperClass=""
         visible={true}
-        ariaLabel="three-circles-rotating"
-        outerCircleColor=""
-        innerCircleColor=""
-        middleCircleColor=""
+        ariaLabel="oval-loading"
+        secondaryColor="#ffffff"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
       />
     </LoaderComponentWrapper>
   );
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from 'components/Header/Header';
-import Loader from 'components/Loader/Loader';
 
 import { Suspense } from 'react';
 import mediumImg from 'images/Rectangle.png.png';
@@ -22,7 +21,7 @@ const DashboardPage = () => {
           <Container>
             <ContainerDashboard>
               <Dashboard />
-              <Suspense fallback={<Loader />}>
+              <Suspense fallback={null}>
                 <Outlet />
               </Suspense>
             </ContainerDashboard>
