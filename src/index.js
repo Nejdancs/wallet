8f75d627cbd215be901b4de15d99bc1c73e8a1da
Sergey Korobka <nejdancs@gmail.com>
2022-12-12 10:11:21 +0000

    add icon filter

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ import { store } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter basename="wallet">
-          <App />
-        </BrowserRouter>
-        {/* </PersistGate> */}
-      </Provider>
-    </ThemeProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter basename="wallet">
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
+    </Provider>
+  </ThemeProvider>
+  // </React.StrictMode>
 );
